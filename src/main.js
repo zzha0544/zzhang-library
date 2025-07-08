8d2eb8319629800398476ee3c49484d7fbcc779d
ZhangZihan <zzha0544@student.monash.edu>
2025-07-08 19:38:38 +0800

    Homework-week3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './assets/main.css'
+// import './style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
